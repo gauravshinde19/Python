9222c1bf19074fed3ab64571258e2139216d1bb0
Gaurav Mahadev Shinde <80934597+gauravshinde19@users.noreply.github.com>
2022-12-08 15:41:52 +0530

    Add files via upload
    This file contains solutions of python programs in a simple way

---
+++ addition.py
@@ -0,0 +1,6 @@
+# Program to accept two integers from user and print addition of that numbers
+
+num1 = float(input("Enter the first number :\n"))
+num2 = float(input("Enter the second number :\n"))
+
+print("Addition of two numbers:\n",num1 + num2)
