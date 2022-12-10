fdb9d54bbb480cf100a2d553f822506548f5ff42
Gaurav Mahadev Shinde <80934597+gauravshinde19@users.noreply.github.com>
2022-12-10 16:09:48 +0530

    Add files via upload

---
+++ area_circle.py
@@ -0,0 +1,4 @@
+# Program to accept radius from user and print area of circle
+
+radius = float(input("Enter the radius of circle :\n"))
+print("Area of circle is:\n",round(radius * radius * 3.14,2))
