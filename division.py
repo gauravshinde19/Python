2855c591939ae424608aa5b71829a23a4080ae03
Gaurav Mahadev Shinde <80934597+gauravshinde19@users.noreply.github.com>
2022-12-12 16:45:16 +0530

    Add files via upload

---
+++ division.py
@@ -0,0 +1,6 @@
+# Program to accept two integers from user and print division of that numbers
+
+num1 = int(input("Enter the first number :\n"))
+num2 = int(input("Enter the second number :\n"))
+
+print("Division of two numbers:\n",round(num1 / num2,2))
